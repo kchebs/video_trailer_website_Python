d71a7ed95b2a7c0fb342023a2350d33b059dbaa4
Kevin <34360653+kchebs@users.noreply.github.com>
2018-05-04 20:35:50 -0700

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,116 @@
+import media
+import website_creator
+
+# Movies
+vvendetta = media.Video("V for Vendetta", 2006, "movie",
+                        "In a future British tyranny, a shadowy freedom "
+                        "fighter, known only by the alias of 'V', plots to "
+                        "overthrow it with the help of a young woman.",
+                        "https://resizing.flixster.com/3X9tgP8PXWJOPuA3_q3tLm7a8GM=/206x305/v1.bTsxMTE2NjczNDtqOzE3NzY5OzEyMDA7ODAwOzEyMDA",
+                        "https://www.youtube.com/watch?v=qxyUl9M_7vc", 8.2, 7.3,
+                        9)
+payitforward = media.Video("Pay it Forward", 2000, "movie", "A young boy "
+                                                            "attempts to make "
+                                                            "the world a "
+                                                            "better place "
+                                                            "after his teacher"
+                                                            " suggests it.",
+                           "https://ia.media-imdb.com/images/M/MV5BNjI3ODI5NDEwMl5BMl5BanBnXkFtZTYwNDYyMjU3._V1_.jpg",
+                           "https://www.youtube.com/watch?v=qfW0wCV9iFI", 7.2,
+                           4, 7.7)
+starwars = media.Video("Star Wars: Episode IV - A New Hope", 1977,
+                       "movie", "Luke Skywalker joins forces with a Jedi "
+                                "Knight, a cocky pilot, a Wookiee and two droids"
+                                " to save the galaxy from the Empire's "
+                                "world-destroying battle-station while also "
+                                "attempting to rescue Princess Leia from the"
+                                " evil Darth Vader.",
+                       "https://ia.media-imdb.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg",
+                       "https://www.youtube.com/watch?v=1g3_CFmnU7k", 8.6,
+                       9.3, 9.6)
+aladdin = media.Video("Aladdin", 1992, "movie", "When a street urchin vies for "
+                                                "the love of a beautiful "
+                                                "princess, he uses a genie's "
+                                                "magic power to make himself off"
+                                                " as a prince in order to marry her.",
+                      "https://upload.wikimedia.org/wikipedia/en/5/58/Aladdinposter.jpg",
+                      "https://www.youtube.com/watch?v=8HrmBXgiwDU", 8, 9.4,
+                      9.2)
+soundofmusic = media.Video("Sound of Music", 1965, "movie", "A woman leaves an "
+                                                            "Austrian convent to"
+                                                            " become a governess"
+                                                            " to the children of"
+                                                            " a Naval officer "
+                                                            "widower around "
+                                                            "World War II.",
+                           "https://upload.wikimedia.org/wikipedia/en/c/c6/Sound_of_music.jpg",
+                           "https://www.youtube.com/watch?v=lEcKXr3mJ_o", 8,
+                           8.6, 9.1)
+dalmations = media.Video("101 Dalmations", 1961, "movie", "When a litter of "
+                                                          "Dalmatian puppies are "
+                                                          "abducted by the "
+                                                          "minions of Cruella de"
+                                                          " Vil, the parents "
+                                                          "must find them before"
+                                                          " she uses them for a "
+                                                          "diabolical fashion "
+                                                          "statement.",
+                         "https://upload.wikimedia.org/wikipedia/en/c/cd/One_Hundred_and_One_Dalmatians_movie_poster.jpg",
+                         "https://www.youtube.com/watch?v=EdV2tPJOPRY",
+                         7.2, 9.8, 7.5)
+marypoppins = media.Video("Mary Poppins", 1964, "movie",
+                          "In turn of the century London, a magical nanny "
+                          "employs music and adventure to help two neglected "
+                          "children become closer to their father.",
+                          "https://upload.wikimedia.org/wikipedia/en/7/78/Marypoppins.jpg",
+                          "https://www.youtube.com/watch?v=nOfH7uEojKk", 7.8,
+                          10, 8.6)
+grease = media.Video("Grease", 1978, "movie", "Good girl Sandy and greaser "
+                                              "Danny fell in love over the "
+                                              "summer. When they unexpectedly "
+                                              "discover they're now in the same "
+                                              "high school, will they be able to"
+                                              " rekindle their romance?",
+                     "https://upload.wikimedia.org/wikipedia/en/e/e2/Grease_ver2.jpg",
+                     "https://www.youtube.com/watch?v=wzWmxjYNfz4", 7.2, 7.6,
+                     8.7)
+homealone = media.Video("Home Alone", 1990, "movie", "An eight-year-old "
+                                                     "troublemaker must protect "
+                                                     "his house from a pair of "
+                                                     "burglars when he is "
+                                                     "accidentally left home "
+                                                     "alone by his family during"
+                                                     " Christmas vacation.",
+                        "https://upload.wikimedia.org/wikipedia/en/7/76/Home_alone_poster.jpg",
+                        "https://www.youtube.com/watch?v=jEDaVHmw7r4", 7.5, 6.2,
+                        7.9)
+hpotterdhtwo = media.Video("Harry Potter and the Deathly Hallows - Part 2",
+                           2011, "movie",
+                           "Harry, Ron, and Hermione search for "
+                           "Voldemort's remaining Horcruxes in their "
+                           "effort to destroy the Dark Lord as the final "
+                           "battle rages on at Hogwarts.",
+                           "https://ia.media-imdb.com/images/M/MV5BMjIyZGU4YzUtNDkzYi00ZDRhLTljYzctYTMxMDQ4M2E0Y2YxXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX667_CR0,0,667,999_AL_.jpg",
+                           "https://www.youtube.com/watch?v=mObK5XD8udk", 8.1,
+                           9.6, 8.9)
+lordringstwo = media.Video("Lord of the Rings: The Two Towers", 2002, "movie",
+                           "While Frodo and Sam edge closer to Mordor "
+                           "with the help of the shifty Gollum, "
+                           "the divided fellowship makes a stand against "
+                           "Sauron's new ally, Saruman, and his hordes of Isengard.",
+                           "https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg",
+                           "https://www.youtube.com/watch?v=cvCktPUwkW0", 8.7,
+                           9.5, 9.5)
+indianajonesark = media.Video("Raiders of the Lost Ark", 1981,
+                              "movie", "Archaeologist and adventurer Indiana "
+                                       "Jones is hired by the U.S. government to"
+                                       " find the Ark of the Covenant before the"
+                                       " Nazis.",
+                              "https://upload.wikimedia.org/wikipedia/en/4/4c/Raiders_of_the_Lost_Ark.jpg",
+                              "https://www.youtube.com/watch?v=XkkzKHCx154",
+                              8.5, 9.4, 9.6)
+
+movies = [vvendetta, payitforward, indianajonesark, aladdin, soundofmusic,
+          dalmations, marypoppins, grease, homealone, lordringstwo,
+          hpotterdhtwo, starwars]
+website_creator.open_movies_page(movies)
---
+++ media.py
@@ -0,0 +1,20 @@
+import webbrowser
+
+
+class Video:
+    def __init__(self, video_title, start_year, video_type, video_storyline,
+                 poster_image, trailer_youtube, imdb_score, rtc_score,
+                 rta_score):
+        self.title = video_title
+        self.year = start_year
+        self.type = video_type
+        self.storyline = video_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.imdb_rating = imdb_score
+        self.rtc_rating = rtc_score
+        self.rta_rating = rta_score
+        self.average_rating = round((imdb_score + rtc_score + rta_score) / 3, 1)
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
